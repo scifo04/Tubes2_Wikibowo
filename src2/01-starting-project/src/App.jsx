daf90f2a9b7d1bf76b97627552748aee5647a426
unknown <117185466+NgokNgok04@users.noreply.github.com>
2024-04-26 21:38:35 +0700

    test

---
+++ src2/01-starting-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+function App() {
+
+  return (
+    <main className="bg-[#8884bc]">
+      <Header />
+
+    </main>
+  );
+}
+
+export default App;
---
+++ src2/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="flex justify-center items-center bg-white p-4 shadow-md">
+      <div className="text-center">
+        <h2 className="text-2xl font-bold text-gray-800 custom-font">
+          Flying Papers
+        </h2>
+      </div>
+    </header>
+  )
+}
+
+export default Header
